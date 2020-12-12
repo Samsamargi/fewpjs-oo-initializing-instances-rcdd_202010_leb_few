1dbfeea40036b68619fede84ee25ab58e61b5540
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2020-12-12 11:21:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,38 +3,27 @@
 class Breakfast{
   constructor(food, drink)
   {
-    this.name=name;
-    this.breed=breed;
+    this.food=food;
+    this.breed=drink;
   }
 } 
-let bigFluffyDog1 = new Dog("Buzz" , "greatPyrenees");
-let bigFluffyDog2 = new Dog("Woody", "labrador");
 
-bigFluffyDog1;
-bigFluffyDog2;
 
 class Lunch{
   constructor(salad , soup , drink)
   {
-    this.name=name;
-    this.breed=breed;
+    this.salad=salad;
+    this.breed=soup;
+    this.drink=drink;
   }
 } 
-let bigFluffyDog1 = new Dog("Buzz" , "greatPyrenees");
-let bigFluffyDog2 = new Dog("Woody", "labrador");
-
-bigFluffyDog1;
-bigFluffyDog2;
 
 class Dinner{
   constructor(salad , soup , entree , _dessert)
   {
-    this.name=name;
-    this.breed=breed;
+    this.salad=salad;
+    this.soup=soup;
+    this.entree=entree;
+    this._dessert=dessert;}
   }
-} 
-let bigFluffyDog1 = new Dog("Buzz" , "greatPyrenees");
-let bigFluffyDog2 = new Dog("Woody", "labrador");
 
-bigFluffyDog1;
-bigFluffyDog2;
